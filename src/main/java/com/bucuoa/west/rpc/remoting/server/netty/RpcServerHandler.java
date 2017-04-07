2eaeab7ee27b851830b8531d9cb1dd0b341306c3
wujiang3 <wujiang3@jd.com>
2017-04-07 18:06:57 +0800

    ok

--- src/main/java/com/bucuoa/west/rpc/remoting/server/netty/RpcServerHandler.java
+++ src/main/java/com/bucuoa/west/rpc/remoting/server/netty/RpcServerHandler.java
@@ -1,4 +1,4 @@
- package com.bucuoa.west.rpc.remoting.server.netty;
+package com.bucuoa.west.rpc.remoting.server.netty;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,49 +13,75 @@ import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
-
 public class RpcServerHandler extends SimpleChannelInboundHandler<RpcRequest> {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(RpcServerHandler.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(RpcServerHandler.class);
 
-    public RpcServerHandler() {
-       
-    }
+	public RpcServerHandler() {
 
-    @Override
-    public void channelRead0(final ChannelHandlerContext ctx, RpcRequest request) throws Exception {
-        // 创建并初始化 RPC 响应对象
-        RpcResponse response = new RpcResponse();
-        response.setRequestId(request.getRequestId());
-        try {
-            Object result = handle(request);
-            
-            response.setResult(result);
-        } catch (Exception e) {
-            LOGGER.error("handle result failure", e);
-            response.setException(e);
-        }
-        // 写入 RPC 响应对象并自动关闭连接
-        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
-    }
+	}
 
-    private Object handle(RpcRequest request) throws Exception {
-        // 获取服务对象
-        String serviceName = request.getInterfaceName();
-        String serviceVersion = request.getServiceVersion();
-        if (StringUtil.isNotEmpty(serviceVersion)) {
-            serviceName += "-" + serviceVersion;
-        }
-		
+	/*
+	 * 覆盖channelActive 方法在channel被启用的时候触发（在建立连接的时候） 覆盖了 channelActive()
+	 * 事件处理方法。服务端监听到客户端活动
+	 */
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		super.channelActive(ctx);
+		LOGGER.debug("channelActive:{}",ctx.name());
+
+	}
+
+	/*
+	 * (non-Javadoc) 覆盖了 handlerAdded() 事件处理方法。 每当从服务端收到新的客户端连接时
+	 */
+	public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+		super.handlerAdded(ctx);
+		LOGGER.debug("handlerAdded:{}",ctx.name());
+	}
+
+	/*
+	 * (non-Javadoc) .覆盖了 handlerRemoved() 事件处理方法。 每当从服务端收到客户端断开时
+	 */
+	public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+		super.handlerRemoved(ctx);
+		LOGGER.debug("handlerRemoved:{}",ctx.name());
+
+	}
+
+	@Override
+	public void channelRead0(final ChannelHandlerContext ctx, RpcRequest request) throws Exception {
+		// 创建并初始化 RPC 响应对象
+		RpcResponse response = new RpcResponse();
+		response.setRequestId(request.getRequestId());
+		try {
+			Object result = handle(request);
+
+			response.setResult(result);
+		} catch (Exception e) {
+			LOGGER.error("handle result failure", e);
+			response.setException(e);
+		}
+		// 写入 RPC 响应对象并自动关闭连接
+		ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+	}
+
+	private Object handle(RpcRequest request) throws Exception {
+		// 获取服务对象
+		String serviceName = request.getInterfaceName();
+		String serviceVersion = request.getServiceVersion();
+		if (StringUtil.isNotEmpty(serviceVersion)) {
+			serviceName += "-" + serviceVersion;
+		}
+
 		String beanid = RemoteServiceCenter.getInterface(serviceName);
 		ProviderStubInvoker serviceInvoker = RemoteServiceCenter.getService(beanid);
-		
-		return serviceInvoker.invoke(request);
-    }
 
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        LOGGER.error("server caught exception", cause);
-        ctx.close();
-    }
+		return serviceInvoker.invoke(request);
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+		LOGGER.error("server caught exception", cause);
+		ctx.close();
+	}
 }
